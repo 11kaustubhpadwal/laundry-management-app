5d8330da6358ffdd4928eaed1e208fd739912d38
Kaustubh Padwal <11kaustubhpadwal@gmail.com>
2020-07-16 01:49:39 +0200

    Responsiveness added to Navbar.

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,20 +1,13 @@
-import React, { Fragment } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import Logo from "../../assets/car.svg";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
   link: {
     textAlign: "center",
     textDecoration: "none",
-    marginTop: "10px",
-    marginLeft: "25px",
-    marginRight: "25px",
-    marginBottom: "20px",
     color: "black",
   },
   logo: {
@@ -26,39 +19,37 @@ const Navbar = () => {
   const classes = useStyles();
 
   return (
-    <Fragment>
-      <Grid
-        container
-        spacing={1}
-        direction="row"
-        justify="center"
-        alignItems="center"
-      >
-        <Grid item xs={1} className={classes.link}>
-          <Link to="/" className={classes.link}>
-            Home
-          </Link>
-        </Grid>
-        <Grid item xs={1} className={classes.link}>
-          <Link to="/about" className={classes.link}>
-            About
-          </Link>
-        </Grid>
-        <Grid item xs={1} className={classes.link}>
-          <img src={Logo} alt="Logo" className={classes.logo}></img>
-        </Grid>
-        <Grid item xs={1} className={classes.link}>
-          <Link to="/register" className={classes.link}>
-            Register
-          </Link>
-        </Grid>
-        <Grid item xs={1} className={classes.link}>
-          <Link to="/login" className={classes.link}>
-            Login
-          </Link>
-        </Grid>
+    <Grid
+      container
+      spacing={1}
+      direction="row"
+      justify="center"
+      alignItems="center"
+    >
+      <Grid item xs={12} sm={2} md={2} lg={2} className={classes.link}>
+        <Link to="/" className={classes.link}>
+          Home
+        </Link>
       </Grid>
-    </Fragment>
+      <Grid item xs={12} sm={2} md={2} lg={2} className={classes.link}>
+        <Link to="/about" className={classes.link}>
+          About
+        </Link>
+      </Grid>
+      <Grid item xs={12} sm={2} md={2} lg={2} className={classes.link}>
+        <img src={Logo} alt="Logo" className={classes.logo}></img>
+      </Grid>
+      <Grid item xs={12} sm={2} md={2} lg={2} className={classes.link}>
+        <Link to="/register" className={classes.link}>
+          Register
+        </Link>
+      </Grid>
+      <Grid item xs={12} sm={2} md={2} lg={2} className={classes.link}>
+        <Link to="/login" className={classes.link}>
+          Login
+        </Link>
+      </Grid>
+    </Grid>
   );
 };
 
