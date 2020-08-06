8c3dfa9a291c49a9d3f6c35f1954adf5f7742f87
Kaustubh Padwal <11kaustubhpadwal@gmail.com>
2020-08-06 20:15:54 +0200

    Project folder structure changed for deployment.

--- server.js
+++ server.js
@@ -22,12 +22,14 @@ app.use("/api/auth", require("./routes/auth/auth"));
 app.use("/api/orders", require("./routes/orders/orders"));
 
 // Serve static assets in production
-// Set static folder
-app.use(express.static(path.join(__dirname, "build")));
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
