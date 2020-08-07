e07c21ff1ce44ca7f9260762fbbe0ec98194dbb3
Kaustubh Padwal <11kaustubhpadwal@gmail.com>
2020-08-07 18:39:42 +0200

    Client side removed.

--- server.js
+++ server.js
@@ -17,16 +17,6 @@ app.use("/api/users", require("./routes/user/user"));
 app.use("/api/auth", require("./routes/auth/auth"));
 app.use("/api/orders", require("./routes/orders/orders"));
 
-// Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
